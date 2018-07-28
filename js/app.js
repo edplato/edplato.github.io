b0b94eb9b191207e06b565307c7d68090259a336
Edward Plato <edplatomail@gmail.com>
2018-07-28 18:00:29 -0400

    add photogallery css grid, wake up servers

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
   console.log("%cThanks for visiting - DevTools much? Me too. -Ed", "color: #465C8B; font-size:22px;");
   navToggle();
+  fetch('https://flickpick.herokuapp.com/', {mode: 'no-cors'})
+    .then((res) => console.log('Wake up Flick Pick heroku live dyno.'));
+  fetch('https://mst3kquickguide.herokuapp.com/', {mode: 'no-cors'})
+    .then((res) => console.log('Wake up MST3K heroku live dyno.'));
 });
 
 function navToggle() {
---
+++ photogallery/gallery.js
@@ -0,0 +1,72 @@
+const gallery = document.querySelector('.gallery');
+const overlay = document.querySelector('.overlay');
+const overlayImage = overlay.querySelector('img');
+const overlayClose = overlay.querySelector('.close');
+
+const photoCount = 14;
+
+// generate number of img boxes per photocount
+function generateHTML(i) {
+  return `
+    <div class="img-box">
+      <img src="photos/${i}.jpg">
+      <div class="img-box-overlay">
+        <button class="view-img">View →</button>
+      </div>
+    </div>
+  `;
+};
+
+// create image boxes to DOM
+const html = Array.from({ length: photoCount }).map((current, i = 1) => generateHTML(i+1)).join('');
+gallery.innerHTML += html;
+const items = document.querySelectorAll('.img-box');
+
+// on click show image overlay modal
+function handleClick(e) {
+  const src = e.currentTarget.querySelector('img').src;
+  overlayImage.src = src;
+  let inner = overlay.children[0];
+  let imgHeight = inner.querySelector('img').naturalHeight;
+  if (imgHeight > 800) {
+    if(window.innerHeight > window.innerWidth){
+      inner.classList.add('tallimg');
+    } else {
+      inner.classList.add('tallimgWide');
+    }
+  }
+  overlay.classList.add('open');
+};
+
+// close overlay modal
+function close() {
+  overlay.classList.remove('open');
+  let inner = overlay.children[0];
+  inner.classList.remove('tallimg');
+  inner.classList.remove('tallimgWide');
+};
+
+// add event listeners
+items.forEach(item => item.addEventListener('click', handleClick));
+
+overlayClose.addEventListener('click', close);
+
+// click off overlay modal to close as well
+overlay.addEventListener('click', function(e) {
+  e.stopPropagation();
+  if (e.target.className === 'overlay open') {
+    close();
+  }
+});
+
+// handle orientation change on mobile to scale tall images
+window.addEventListener("orientationchange", function() {
+  let inner = overlay.children[0];
+  if (inner.classList.contains('tallimg')) {
+    inner.classList.remove('tallimg');
+    inner.classList.add('tallimgWide');
+  } else if (inner.classList.contains('tallimgWide')) {
+    inner.classList.remove('tallimgWide');
+    inner.classList.add('tallimg');
+  }
+}, false);
